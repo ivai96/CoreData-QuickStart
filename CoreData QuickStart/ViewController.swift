6341185083154637e250259c35b282abe06596c6
Ivana Krivchevska <i.krivcevska@gmail.com>
2021-07-26 18:32:49 +0200

    Initial Commit

---
+++ CoreData QuickStart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData QuickStart
+//
+//  Created by Ivana Krivchevska on 7/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
